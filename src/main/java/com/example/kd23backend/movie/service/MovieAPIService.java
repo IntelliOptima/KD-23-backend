ebc2332b5e485f5190825c623a851a11c49978f5
Alex793x <alex793x@stud.kea.dk>
2023-10-05 11:30:22 +0200

    finilized setup for fetch

--- src/main/java/com/example/kd23backend/movie/service/MovieAPIService.java
+++ src/main/java/com/example/kd23backend/movie/service/MovieAPIService.java
@@ -1,9 +1,6 @@
 package com.example.kd23backend.movie.service;
 
-import com.example.kd23backend.movie.model.Actor;
 import com.example.kd23backend.movie.model.Movie;
-import com.example.kd23backend.movie.repository.ActorRepository;
-import com.example.kd23backend.movie.repository.GenreRepository;
 import com.example.kd23backend.movie.repository.MovieRepository;
 import org.json.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
@@ -18,24 +15,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class MovieAPIService implements IMovieAPIService {
 
-    private final MovieRepository movieRepository;
-    private final ActorRepository actorRepository;
     final String BASE_MOVIE_API_URL = "https://api.themoviedb.org/3/movie/";
     final String API_KEY = "?api_key=bf45e26b2cbe79b9bcb399d646313e59&";
     final String APPEND_TO_MOVIE = "append_to_response=videos,credits";
-    final String BEARER = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjQ1ZTI2YjJjYmU3OWI5YmNiMzk5ZDY0NjMxM2U1OSIsInN1YiI6IjY1MTNlZDg0Y2FkYjZiMDJiZTU0OWYwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VeSYNMtKMSrQskLEz_ZNU6f4p160eIGRYaB7m31hUHM\"";
     InputStream inputStream = getClass().getResourceAsStream("/movie_ids_10_03_2023.json");
-    private final RestTemplate restTemplate;
 
-    public MovieAPIService(RestTemplate restTemplate, MovieRepository movieRepository, ActorRepository actorRepository, GenreRepository genreRepository, ActorRepository actorRepository1) {
+    private final RestTemplate restTemplate;
+    private final MovieRepository movieRepository;
+
+
+    public MovieAPIService(RestTemplate restTemplate, MovieRepository movieRepository) {
         this.restTemplate = restTemplate;
         this.movieRepository = movieRepository;
-        this.actorRepository = actorRepository1;
     }
 
 
@@ -51,7 +46,7 @@ public class MovieAPIService implements IMovieAPIService {
                 JSONObject json = new JSONObject(line);
                 int movieId = json.getInt("id");
 
-                if (movieId > 20000) {
+                if (movieId > 50000) {
                     break;
                 }
 
@@ -65,18 +60,11 @@ public class MovieAPIService implements IMovieAPIService {
 
             }
             movieRepository.saveAll(movies);
-
+            System.out.println("Next batch of movies has been added!");
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
-
-    public void skipFirstFourMovies(BufferedReader br) throws IOException {
-        for (int i = 0; i < 5; i++) {
-            br.readLine();
-        }
-    }
-
 
     public Movie fetchOneMovie(Integer id) {
         try {
@@ -85,15 +73,22 @@ public class MovieAPIService implements IMovieAPIService {
                     baseURLWithID(id),
                     HttpMethod.GET,
                     null,
-                    new ParameterizedTypeReference<Movie>() {
+                    new ParameterizedTypeReference<>() {
                     }
             );
 
             return movieResponse.getBody();
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println(e.getMessage());
         }
         return null;
+    }
+
+
+    public void skipFirstFourMovies(BufferedReader br) throws IOException {
+        for (int i = 0; i < 5; i++) {
+            br.readLine();
+        }
     }
 
 
